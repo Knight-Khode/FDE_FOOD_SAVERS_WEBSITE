8637fbb7f2057f6ab9d10a4223cd1680c6ed0645
Mohammed  Elmir <mohammed.elmir>
2021-09-18 18:27:31 +0000

    fetching data with backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import {Row,Col,ListGroup,Card,Button,Image} from "react-bootstrap"
 import Rating from '../components/Rating'
-import products from "../products"
 
 const ProductScreen = ({match}) => {
-    const product = products.find(p=>p._id===match.params.id)
+    const [product,setProduct]=useState({})
+
+    useEffect(()=>{
+        const fetchProduct= async ()=>{
+            const {data} = await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+
+        fetchProduct()
+    },[])
+
     return (
         <>
             <Link className="btn btn-dark my-3" to="/">
