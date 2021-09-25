63e382214c1ac1dad2217f9535db0d3b7e9d3cbc
Mohammed  Elmir <mohammed.elmir>
2021-09-25 17:43:02 +0000

    added some data

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express  from "express"
 import dotenv from "dotenv"
 import products from  "./data/products.js"
 import connectDB from "./config/db.js"
+import User from "./models/userModel.js"
+import Product from "./models/productModel.js"
+import Order from "./models/orderModel.js"
+import users from "./data/users.js"
 
 dotenv.config()
 
@@ -21,6 +25,47 @@ app.get("/api/products/:id",(req,res)=>{
     const product = products.find(p=>p._id===req.params.id)
     res.json(product)
 })
+
+
+const importData = async ()=>{
+    try{
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        const createdUsers= await User.insertMany(users)
+
+        const adminUser = createdUsers[0]._id
+
+        const sampleProducts = products.map(product =>{
+            return {...product,user:adminUser}
+        })
+
+        await Product.insertMany(sampleProducts)
+
+        console.log("Data Imported!")
+        process.exit()
+    }catch(error){
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+const destroyData = async ()=>{
+    try{
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        console.log("Data Destroyed!")
+        process.exit()
+    }catch(error){
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+importData()
 
 const PORT = process.env.PORT || 5000
 
